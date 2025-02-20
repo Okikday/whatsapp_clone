1ead16b1587f010fafe24d67059596e0dc816ad8
Okikiola <okikday@gmail.com>
2025-02-20 05:45:19 +0100

    Delete test_app directory

--- test_app/lib/services/dio_services.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dio/dio.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-
-
-
-//General API URL
-final String apiURL = dotenv.env['API_URL'] ?? "";
-final Dio dio = Dio();
